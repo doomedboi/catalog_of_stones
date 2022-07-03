3292db2256a579d0caca494c7af66015f006a5e6
doomedboi <akulenko.vv@students.dvfu.ru>
2022-07-03 23:02:06 +1000

    database models
    & communicate with server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const sequelize = require('./database')
 const cors = require('cors')
 const router = require('./routes/index')
 
+const models = require('./models/models')
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -12,6 +13,14 @@ app.use('/api', router)
 
 /* connect to database is there */
 
-app.listen(port, () => {
-    console.log("Server started on the 5001 port")
-})
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => {console.log('server started')})
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
